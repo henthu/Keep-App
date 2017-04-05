f3e28fe09d2d83f01a8ad96b2cd260853343ab59
henthu <mohamedamine.hamdi@gmail.com>
2017-04-05 18:41:09 +0200

    First push

---
+++ KeepUp-App/Model/Goal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Goal
+    {
+        public String Name { get; set; }
+        public enum Priority
+        {
+            CRITICAL,
+            HIGH,
+            MEDIUM,
+            LOW
+        }
+
+        public DateTime Ultimatum { get; set; }
+
+    }
+}
